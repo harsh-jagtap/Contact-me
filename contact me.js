1f8477d76009dcb9eaf64bcabc654b0970ee3a7c
Harshwardhan Jagatp <harshwardhanjagtap2007@gmail.com>
2021-08-12 08:47:30 +0530

    contact information is saved in server

--- contact me.js
+++ contact me.js
@@ -8,7 +8,6 @@ let button2 = document.getElementById("submitExperiense");
 // information 
 button1.addEventListener("click", function(e) {
     // button to normal
-    e.preventDefault();
     main.innerHTML = `
     <div id="fun" class="box font-Mid">Thanks for submiting
     <div>Your information is not used it is only for testing purpose and how is you experience on this website write down below.</div>
@@ -19,30 +18,33 @@ button1.addEventListener("click", function(e) {
     </div>
     `
 
-    // for experience
-    button2.addEventListener("click", function(e) {
-        e.preventDefault();
-        main.innerHTML = `
-    <div class="box">
-        <h1>Contact Menu</h1>
-        <hr>
-        <form action="submit">
-            <!-- name -->
-            <label for="fullName">Full Name</label>
-            <input id="fullName" class="rectInput" type="text" placeholder="Enter your name">
-            <!-- email -->
-            <label for="email">Email</label>
-            <input id="email" type="email" class="rectInput" placeholder="Enter your email">
-            <!-- phone no -->
-            <label for="phone">Phone no</label>
-            <input id="phone" type="tel" class="rectInput" placeholder="Enter your phone no">
-            <!-- reason -->
-            <label for="reason">Reason of contact</label>
-            <textarea id="reason" class="rectInput" cols="30" rows="5" placeholder="Enter your reason for contact"></textarea>
-            <!-- submit -->
-            <button id="submitForm" class="rectInput">Submit</button>
-        </form>
-    </div>
-    `
-    });
 });
+
+
+// for experience
+
+// button2.addEventListener("click", function(e) {
+//     e.preventDefault();
+//     main.innerHTML = `
+// <div class="box">
+//     <h1>Contact Menu</h1>
+//     <hr>
+//     <form action="submit">
+//         <!-- name -->
+//         <label for="fullName">Full Name</label>
+//         <input id="fullName" class="rectInput" type="text" placeholder="Enter your name">
+//         <!-- email -->
+//         <label for="email">Email</label>
+//         <input id="email" type="email" class="rectInput" placeholder="Enter your email">
+//         <!-- phone no -->
+//         <label for="phone">Phone no</label>
+//         <input id="phone" type="tel" class="rectInput" placeholder="Enter your phone no">
+//         <!-- reason -->
+//         <label for="reason">Reason of contact</label>
+//         <textarea id="reason" class="rectInput" cols="30" rows="5" placeholder="Enter your reason for contact"></textarea>
+//         <!-- submit -->
+//         <button id="submitForm" class="rectInput">Submit</button>
+//     </form>
+// </div>
+// `
+// });
